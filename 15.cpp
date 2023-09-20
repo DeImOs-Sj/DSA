db11da5833cd2a81a6724d43ddd3fbb23e22d967
Your Name <shlokjagtap123@gmail.com>
2023-09-20 15:08:50 +0530

    done some patterns

--- 15.cpp
+++ 15.cpp
@@ -2,14 +2,14 @@
 using namespace std;
 int main ()
 {
-    for(int i=0;i<4;i++)
+    for(int i=0;i<=5;i++)
     {
-        for(int j=0;j<4;j++)
+        for(int j=0;j<=i-1;j++)
         {
             cout<<"*";
         }
         cout<<endl;
-    }
 
-return 0;
+    }
+    return 0;
 }
---
+++ 16.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main ()
+{
+    for(int i=0;i<5;i++)
+    { 
+        cout<<"1";
+        for(int j=2;j<=i+1;j++)
+        {
+            cout<<j;
+        }
+        cout<<endl;
+
+    }
+    return 0;
+}
