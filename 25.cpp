1d7b74a69bcf662f482b42d2b7c1aa0cf7541926
Your Name <shlokjagtap123@gmail.com>
2023-09-24 16:05:52 +0530

    STL

---
+++ 25.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std ;
+void print()
+{
+    cout<<"SHLOK";
+}
+int sum(int a,int b)
+{
+    return a+b;
+}
+int main()
+{
+print();
+int s=sum(1,5);
+cout<<s<<endl;
+return 0;
+}
