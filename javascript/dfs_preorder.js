b4e518c370a966680e199468a86f77df0782d623
shlok <shlokjagtap123@gmail.com>
2026-04-13 13:40:18 +0530

    height of tree using dfs

--- javascript/dfs_preorder.js
+++ javascript/dfs_preorder.js
@@ -1,26 +1,40 @@
+
 function TreeStructure(val){
     this.val = val
     this.left = null
     this.right = null
 }
 
+function c(root){
+
+if (root === null) return 0;
 
 
-function height(root){
-    if (root === null) return 0;  // ✅ base case
+   c(root.left)
+       console.log(root.val)
 
-    let leftHeight = height(root.left);
-    let rightHeight = height(root.right);
+ c(root.right)
 
-    return 1 + Math.max(leftHeight, rightHeight);
+
+
 }
+
+
 let root = new TreeStructure(1)
 root.left = new TreeStructure(2)
 root.right = new TreeStructure(3)
 root.left.left = new TreeStructure(4)
-root.left.left.left = new TreeStructure(69)
 root.left.right = new TreeStructure(5)
 root.right.left = new TreeStructure(6)
 root.right.right = new TreeStructure(7)
 
-console.log(height(root))
+
+c(root)
+
+
+//        1
+//       / \
+//     2    3
+//   /  \   / \
+// 4     5 6   7
+
